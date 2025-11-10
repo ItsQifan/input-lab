72644d3b3d86060bdb9107baf961b738665120d3
zhouchuanxiang <zhouchuanxiang-fx@bestpay.com.cn>
2025-11-10 16:14:03 +0800

    first commit

---
+++ input-lab-wheels/src/main/java/com/zhouchuanxiang/inputlab/wheels/InputLabWheelsApplication.java
@@ -0,0 +1,13 @@
+package com.zhouchuanxiang.inputlab.wheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InputLabWheelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InputLabWheelsApplication.class, args);
+	}
+
+}
---
+++ input-lab-wheels/src/test/java/com/zhouchuanxiang/inputlab/wheels/InputLabWheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouchuanxiang.inputlab.wheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InputLabWheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
